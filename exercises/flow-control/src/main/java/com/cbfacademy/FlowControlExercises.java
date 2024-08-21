f8b087ec0811b913a59f901f62ede129b18809ef
Sophia John <smc_john@hotmail.com>
2024-08-20 20:58:40 -0400

    FlowControl Exercises

--- exercises/flow-control/src/main/java/com/cbfacademy/FlowControlExercises.java
+++ exercises/flow-control/src/main/java/com/cbfacademy/FlowControlExercises.java
@@ -1,58 +1,74 @@
 package com.cbfacademy;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
 public class FlowControlExercises {
 
     public List<String> fizzBuzz(List<Integer> numbers) {
-        // TODO - Implement this method such that
-        //  it creates a list where for each element of the input list ${numbers}
         List<String> result = new ArrayList<>();
-        for (Integer number : numbers) {}
-        //  - if the ${element} is divisible by 3, it adds “Fizz” to the list
-            if (number % 3 == 0) {
-                result.add("Fizz");
-            }
-        //  - if the ${element} is divisible by 5, it adds “Buzz” to the list
-            else if (number % 5 == 0) {
-                result.add("Buzz");
-            }
-        //  - if the ${element} is divisible by both 3 and 5, it adds “FizzBuzz” to the list
-            else if (number % 3 == 0 && number % 5 == 0) {
+        for (Integer number : numbers) {
+            if (number % 3 == 0 && number % 5 == 0) {
                 result.add("FizzBuzz");
-            }
-        //  - it adds the element to the list in any other case
-            else {
+            } else if (number % 3 == 0) {
+                result.add("Fizz");
+            } else if (number % 5 == 0) {
+                result.add("Buzz");
+            } else {
                 result.add(number.toString());
             }
-        //  - it returns the constructed list
-        throw new RuntimeException("Not implemented");
-    }
+        }
+        return result; 
+}
 
     public String whichMonth(Integer number) {
         // TODO - Implement this method such that
         //  - it returns the month corresponding to the input ${number}
         //  - if the ${number} is invalid, the method should return "Invalid month number"
-        throw new RuntimeException("Not implemented");
+        //throw new RuntimeException("Not implemented");
+        String month;
+        switch (number) {
+            case 1: month = "January";  break;  
+            case 2: month = "February";  break;
+            case 3: month = "March";  break;
+            case 4: month = "April";  break;
+            case 5: month = "May";  break;
+            case 6: month = "June";  break;
+            case 7: month = "July";  break;
+            case 8: month = "August";  break;
+            case 9: month = "September";  break;
+            case 10: month = "October";  break;
+            case 11: month = "November";  break;
+            case 12: month = "December";  break;
+            default: month = "Invalid month number";  break;
+        }
+        return month;
     }
 
     public Map<String, Integer> sumOfOddsAndSumOfEvens() {
-        // TODO - Implement this method such that
-        //  - creates and initialises a list of 100 numbers - from 1 to 100
-        //  - determines the sum of all the even numbers in the list
-        //  - determines the sum of all the odd numbers in the list
-        //  - returns a map with two entries:
-        //      {"SumOfEvens", calculatedSumOfEvens}, {"SumOfOdds", calculatedSumOfOdds}
-        throw new RuntimeException("Not implemented");
+        List<Integer> numbers = new ArrayList<>();
+
+        for(int i = 1; i <= 100; i++) {
+            numbers.add(i);
+        }
+        int sumOfEvens = 0;
+        int sumOfOdds = 0;
+        for (Integer number : numbers) {
+            if (number % 2 == 0) {
+                sumOfEvens += number;
+            } else {
+                sumOfOdds += number;
+            }
+        }
+        return Map.of("sumOfEvens", sumOfEvens, "sumOfOdds", sumOfOdds);
     }
 
     public List<Integer> reverse(ArrayList<Integer> numbers) {
-        // TODO - Implement this method such that
-        //  - it takes an array list of integers
-        //  - it returns the list in reverse order
-        throw new RuntimeException("Not implemented");
+        List<Integer> reversedList = new ArrayList<>();
+        Collections.reverse(reversedList);
+        return reversedList;
     }
 
     public String getName() {
